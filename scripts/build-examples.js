74a1ea4d87621bbbba0e8235f476385d833b30cc
Chris Klimas <chris@twinery.org>
2018-01-27 23:29:45 -0500

    Title case examples

--- scripts/build-examples.js
+++ scripts/build-examples.js
@@ -7,16 +7,17 @@ const pify = require('pify');
 const exec = require('child-process-promise').exec;
 const path = require('path');
 const fs = pify(require('fs'));
+const titleCase = require('title-case');
 const pkg = require('../package.json');
 
 fs.readdir(path.resolve('examples')).then(files => {
 	files.filter(f => path.extname(f) === '.txt').forEach(f => {
-		const name = path.parse(f).name;
+		const name = titleCase(path.parse(f).name);
 		const src = path.join('examples', f);
 		const output = src.replace('.txt', '.html');
 
 		console.log(path.basename(f));
 
-		exec(`entwine --format dist/${pkg.name.toLowerCase()}-${pkg.version}/format.js --start Start ${src} --name ${name} > ${output}`);
+		exec(`entwine --format dist/${pkg.name.toLowerCase()}-${pkg.version}/format.js --start Start ${src} --name "${name}" > ${output}`);
 	});
 });
