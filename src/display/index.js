32d24d8e0df22414f17b6472e1fae6d6f972b5fc
Chris Klimas <chris@twinery.org>
2018-07-10 12:46:47 -0400

    Add back in visual transitions
    Now header & footer also transition content

--- src/display/index.js
+++ src/display/index.js
@@ -11,21 +11,30 @@
 
 import closest from 'closest';
 import event from '../event';
+import {crossfade, fadeInOut, none} from './transitions';
 import {get} from '../state';
 import {go, restart} from '../author/actions';
 import {passageNamed} from '../story';
 import {render} from '../template';
 import {validate} from './inputs';
+import './index.scss';
 
 let mainContent, marginals;
+const transitions = {crossfade, fadeInOut, none};
 
 export const defaults = {
+	'config.body.transition.name': 'crossfade',
+	'config.body.transition.duration': '500ms',
 	'config.header.left': '',
 	'config.header.center': '',
 	'config.header.right': '',
+	'config.header.transition.name': 'crossfade',
+	'config.header.transition.duration': '500ms',
 	'config.footer.left': '_`story.name()`_',
 	'config.footer.center': '',
-	'config.footer.right': "`link('Restart').restart()`"
+	'config.footer.right': "`link('Restart').restart()`",
+	'config.footer.transition.name': 'crossfade',
+	'config.footer.transition.duration': '500ms'
 };
 
 function attachDomListeners(el) {
@@ -60,6 +69,72 @@ function attachDomListeners(el) {
 	});
 }
 
+// It is of course possible for variables, including the trail, to update
+// multiple times in one JavaScript execution stack. We use flags to prevent
+// multiple updates to the DOM in one stack, since it's inefficient and can lead
+// to odd display.
+
+let updateNeeded = false;
+let updateTrail = false;
+
+function updateDom() {
+	if (!updateNeeded) {
+		return;
+	}
+
+	// Update the trail if needed.
+
+	if (updateTrail) {
+		const trail = get('trail');
+		const bodyTransition = get('config.body.transition.name');
+
+		if (transitions[bodyTransition]) {
+			transitions[bodyTransition](
+				mainContent,
+				render(passageNamed(trail[trail.length - 1]).source),
+				get('config.body.transition.duration')
+			);
+		} else {
+			transitions.none(
+				mainContent,
+				render(passageNamed(trail[trail.length - 1]).source)
+			);
+		}
+	}
+
+	// Always update header and footer, because we don't know what their
+	// contents may depend on.
+
+	['header', 'footer'].forEach(m => {
+		marginals[m].container.classList.remove('has-content');
+
+		['left', 'center', 'right'].forEach(part => {
+			const html = render(get(`config.${m}.${part}`));
+
+			if (html !== '') {
+				marginals[m].container.classList.add('has-content');
+			}
+
+			const marginalTransition = get(`config.${m}.transition.name`);
+
+			if (transitions[marginalTransition]) {
+				transitions[marginalTransition](
+					marginals[m][part],
+					html,
+					get(`config.${m}.transition.duration`)
+				);
+			} else {
+				transitions.none(marginals[m][part], html);
+			}
+		});
+	});
+
+	// Reset flags.
+
+	updateNeeded = false;
+	updateTrail = false;
+}
+
 export function init() {
 	mainContent = document.querySelector('#page article');
 	marginals = {};
@@ -74,29 +149,15 @@ export function init() {
 	attachDomListeners(document.body);
 
 	event.on('state-change', ({name, value}) => {
-		// Update the main content when the current passage has changed.
+		updateNeeded = true;
 
 		if (name === 'trail') {
-			mainContent.innerHTML = render(
-				passageNamed(value[value.length - 1]).source
-			);
+			updateTrail = true;
 		}
 
-		// Always update header and footer, because we don't know what their
-		// contents may depend on.
+		// Delay execution of updateDom until after the current execution stack
+		// completes.
 
-		['header', 'footer'].forEach(m => {
-			marginals[m].container.classList.remove('has-content');
-
-			['left', 'center', 'right'].forEach(part => {
-				const html = render(get(`config.${m}.${part}`));
-
-				if (html !== '') {
-					marginals[m].container.classList.add('has-content');
-				}
-
-				marginals[m][part].innerHTML = html;
-			});
-		});
+		Promise.resolve().then(updateDom);
 	});
 }
