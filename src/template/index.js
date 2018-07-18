fd403291f56a4e775cbd20b03f8c36c996726acd
Chris Klimas <chris@twinery.org>
2018-07-18 00:12:42 -0400

    Properly connect default modifiers

--- src/template/index.js
+++ src/template/index.js
@@ -1,8 +1,13 @@
 // The top-level interface for template rendering.
 
+import modifiers from './modifiers';
 import parse from './parse';
 import renderParsed from './render';
 import {get, set} from '../state';
+
+export const defaults = {
+	'config.template.modifiers': modifiers
+};
 
 export function render(source, ignoreVars = false) {
 	return renderParsed(
--- src/template/modifiers/index.js
+++ src/template/modifiers/index.js
@@ -3,9 +3,4 @@ import Append from './append';
 import Conditionals from './conditionals';
 import Continue from './continue';
 
-export function addBuiltins(renderer) {
-	renderer.addModifier('after', After);
-	renderer.addModifier('append', Append);
-	renderer.addModifier('conditionals', Conditionals);
-	renderer.addModifier('continue', Continue);
-}
+export default [After, Append, Conditionals, Continue];
