1d1cb77c35d74f4afe348699b1b5883502477964
Chris Klimas <chris@twinery.org>
2018-09-17 23:03:46 -0400

    Add note modifier

--- src/template/modifiers/index.js
+++ src/template/modifiers/index.js
@@ -3,5 +3,6 @@ import Align from './align';
 import Append from './append';
 import Conditionals from './conditionals';
 import Continue from './continue';
+import Note from './note';
 
-export default [After, Align, Append, Conditionals, Continue];
+export default [After, Align, Append, Conditionals, Continue, Note];
---
+++ src/template/modifiers/note.js
@@ -0,0 +1,11 @@
+/*
+This modifier allows entering text that won't be displayed to the player. It can
+be written as [note], [note to self], [n.b.], [nb], [todo], or [fixme].
+*/
+
+export default {
+	match: /^(note(\s+to\s+self)?|n?b?)|todo|fixme$/i,
+	process(output) {
+		output.text = '';
+	}
+};
