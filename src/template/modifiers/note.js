f354987b1723a47f10e57bc2535f813032a7c2d9
Chris Klimas <chris@twinery.org>
2018-10-07 20:57:24 -0400

    Fix regexp for note modifier

--- src/template/modifiers/note.js
+++ src/template/modifiers/note.js
@@ -4,7 +4,7 @@ be written as [note], [note to self], [n.b.], [nb], [todo], or [fixme].
 */
 
 export default {
-	match: /^(note(\s+to\s+self)?|n?b?)|todo|fixme$/i,
+	match: /^(note(\s+to\s+self)?|n?b?|todo|fixme)$/i,
 	process(output) {
 		output.text = '';
 	}
