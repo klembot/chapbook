8c6755aeb1222ec86e6acce171a40b2cc7d27f01
Chris Klimas <chris@twinery.org>
2018-10-07 20:25:22 -0400

    Fix link line styling in Safari

--- src/style/index.js
+++ src/style/index.js
@@ -13,9 +13,9 @@ export const defaults = {
 	'config.style.page.font': 'Iowan Old Style/Constantia/Georgia/serif 18',
 	'config.style.page.color': 'gray-9 on white',
 	'config.style.page.link.color': 'gray-9',
-	'config.style.page.link.lineColor': 'red-5',
+	'config.style.page.link.lineColor': 'red-8',
 	'config.style.page.link.font': 'underline',
-	'config.style.page.link.active.color': 'red-5 on red-0',
+	'config.style.page.link.active.color': 'red-8 on red-0',
 	'config.style.page.header.font': '16',
 	'config.style.page.header.link.font': 'small caps',
 	'config.style.page.footer.font': '16',
@@ -182,12 +182,16 @@ export function init() {
 					style(selector, parseColor(value));
 					break;
 
-				case 'linecolor':
+				case 'linecolor': {
 					log(`Setting line color for ${selector}`);
+					const colorValue = parseColor(value).color;
+
 					style(selector, {
-						'text-decoration-color': parseColor(value).color
+						'text-decoration-color': colorValue,
+						'-webkit-text-decoration-color': colorValue
 					});
 					break;
+				}
 
 				case 'font':
 					log(`Setting font for ${selector}`);
