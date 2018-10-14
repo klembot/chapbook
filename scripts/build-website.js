0ee9e653610ac2f5daa87a3c10d85a01f7a78211
Chris Klimas <chris@twinery.org>
2018-10-14 13:57:06 -0400

    Add docs/ directory, script to generate web site, script to package a release

---
+++ scripts/build-website.js
@@ -0,0 +1,30 @@
+const pify = require('pify');
+const cpy = require('cpy');
+const fs = pify(require('fs'));
+const makeDir = require('make-dir');
+const pug = require('pug');
+
+const pkg = require('../package.json');
+
+const encoding = {encoding: 'utf8'};
+
+makeDir('docs')
+	.then(() =>
+		fs.writeFile(
+			'docs/index.html',
+			pug.renderFile('homepage/index.pug', pkg)
+		)
+	)
+	.then(() => cpy('homepage/*{.css,.jpeg}', 'docs'))
+	.then(() => makeDir('docs/examples'))
+	.then(() => cpy('examples/cloak-of-darkness.*', 'docs/examples'))
+	.then(() => makeDir('docs/guide'))
+	.then(() => cpy('guide/_book/**', 'docs/guide'))
+	.then(() => makeDir(`docs/use/${pkg.version}`))
+	.then(() =>
+		cpy(
+			`dist/chapbook-${pkg.version}/format.js`,
+			`docs/use/${pkg.version}/`
+		)
+	)
+	.then(() => console.log('Wrote files to docs/.'));
