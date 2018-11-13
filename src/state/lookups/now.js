802181f6fbc36481395d1efb5c9ea5b44ca2cb95
Chris Klimas <chris@twinery.org>
2018-11-12 23:16:40 -0500

    Add descriptive date lookups

--- src/state/lookups/now.js
+++ src/state/lookups/now.js
@@ -23,11 +23,27 @@ export default function init(setLookup) {
 		return new Date().getDay() + 1;
 	});
 
+	setLookup('now.weekdayName', () => {
+		return new Date().toLocaleString(navigator.language, {weekday: 'long'});
+	});
+
 	setLookup('now.month', () => {
 		return new Date().getMonth() + 1;
 	});
 
+	setLookup('now.monthName', () => {
+		return new Date().toLocaleString(navigator.language, {month: 'long'});
+	});
+
 	setLookup('now.year', () => {
 		return new Date().getFullYear();
+	});
+
+	setLookup('now.timestamp', () => {
+		return new Date().toLocaleString(navigator.language, {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric'
+		});
 	});
 }
