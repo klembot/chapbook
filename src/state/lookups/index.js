a549e7cec0705d21d58bdeef73cf50f1cd2d143d
Chris Klimas <chris@twinery.org>
2019-03-20 20:59:25 -0400

    Add passage.name and passage.visits lookup

--- src/state/lookups/index.js
+++ src/state/lookups/index.js
@@ -1,5 +1,6 @@
 import initBrowser from './browser';
 import initNow from './now';
+import initPassage from './passage';
 import initRandom from './random';
 import initStoryName from './story-name';
 import {setLookup} from '../index';
@@ -7,6 +8,7 @@ import {setLookup} from '../index';
 export default function init() {
 	initBrowser(setLookup);
 	initNow(setLookup);
+	initPassage(setLookup);
 	initRandom(setLookup);
 	initStoryName(setLookup);
 }
