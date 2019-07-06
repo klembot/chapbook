6607bc8acf74e719271ae2229f3b012df165ee94
Chris Klimas <chris@twinery.org>
2019-07-06 12:23:27 -0400

    Fix warnings

--- src/display/warnings.js
+++ src/display/warnings.js
@@ -5,23 +5,36 @@ passage.
 
 import escape from 'lodash.escape';
 import {get} from '../state';
+import event from '../event';
+import './warnings.scss';
 
 export function init() {
 	const nativeWarn = window.console.warn;
+	const container = document.querySelector('#page .warnings');
 
 	console.warn = (...messages) => {
 		if (get('config.testing')) {
-			const container = document.querySelector('#page article div');
+			try {
+				container.removeAttribute('hidden');
+				messages.forEach(message => {
+					const li = document.createElement('li');
 
-			messages.forEach(message => {
-				const p = document.createElement('p');
-
-				p.className = 'warning';
-				p.innerHTML = `Warning: ${escape(message)}`;
-				container.appendChild(p);
-			});
+					li.className = 'warning';
+					li.innerHTML = `Warning: ${escape(message)}`;
+					container.appendChild(li);
+				});
+			} catch (err) {
+				/* Fail silently-- it'll still appear in the browser console. */
+			}
 		}
 
 		nativeWarn.apply(window.console, messages);
 	};
+
+	event.on('state-change', ({name}) => {
+		if (name === 'trail') {
+			container.setAttribute('hidden', '');
+			container.innerHTML = '';
+		}
+	});
 }
