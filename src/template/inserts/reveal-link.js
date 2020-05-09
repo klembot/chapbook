a4759b313f80f1ccca098ff9f02516dd530ce699
Chris Klimas <chris@twinery.org>
2020-05-09 19:20:24 -0400

    fix: improve ordering when inserting paragraphs

--- src/template/inserts/reveal-link.js
+++ src/template/inserts/reveal-link.js
@@ -35,6 +35,33 @@ export default {
 	},
 };
 
+/*
+Finds the first parent of an element that has block styling. If none exists,
+this returns null.
+*/
+
+function findBlockParent(el) {
+	let parent = el.parentNode;
+
+	while (parent) {
+		let display = window.getComputedStyle(parent).display;
+
+		if (
+			display === 'block' ||
+			display === 'flex' ||
+			display === 'inline-block' ||
+			display === 'table' ||
+			display === 'list-item'
+		) {
+			return parent;
+		}
+
+		parent = parent.parentNode;
+	}
+
+	return null;
+}
+
 event.on('dom-click', el => {
 	let source = el.dataset.cbRevealText;
 
@@ -45,32 +72,68 @@ event.on('dom-click', el => {
 	if (source) {
 		const output = document.createElement('div');
 
-		output.innerHTML = render(source);
+		/*
+		Need to trim() this to avoid spurious empty text nodes at the end.
+		*/
+
+		output.innerHTML = render(source).trim();
+
+		/*
+		Output contains only block-level elements as its children. We know this
+		because when we render source, it always comes wrapped in block
+		elements; a bare link in a paragraphy by itself, for example, gets
+		rendered with a <p> container by marked.
+		*/
 
 		changeBody(() => {
-			/*
-			If the first node of the output is a <p>, substitute it inline for
-			the link.
-			*/
+			const toInsert = output.children.length;
 
-			if (output.firstChild.nodeName === 'P') {
-				const text = document.createElement('span');
+			if (toInsert > 0) {
+				/*
+				Put the first child where the link was in the DOM. Set its display
+				as inline to make it imitate the link's layout.
+				*/
 
-				text.innerHTML = output.firstChild.innerHTML;
-				el.parentNode.insertBefore(text, el.nextSibling);
+				const firstInsert = document.createElement('span');
+
+				firstInsert.innerHTML = output.firstChild.innerHTML;
+				el.parentNode.insertBefore(firstInsert, el);
 				output.removeChild(output.firstChild);
-			}
 
-			/*
-			Append the rest of the output after the node that the link resides
-			in-- in most cases, after the paragraph that the link is in.
-			*/
+				if (toInsert > 1) {
+					/*
+					If there are other block elements, place them as siblings of the
+					parent.
+					*/
 
-			while (output.firstChild) {
-				el.parentNode.parentNode.insertBefore(
-					output.firstChild,
-					el.parentNode.nextSibling
-				);
+					const lastInsert = output.lastChild;
+
+					while (output.lastChild) {
+						// if (output.innerHTML !== '') {
+						// 	console.log('setting lastInsert', output.firstChild);
+						// 	lastInsert = output.firstChild;
+						// }
+
+						el.parentNode.parentNode.insertBefore(
+							output.lastChild,
+							el.parentNode.nextSibling
+						);
+					}
+
+					/*
+					Move any inline elements after the link we just expanded to
+					the end of the last newly-inserted block element. Otherwise,
+					they'll be sandwiched in by the new insert and order of
+					content won't be preserved.
+					*/
+
+					console.log(lastInsert, lastInsert.innerHTML);
+
+					while (el.nextSibling) {
+						console.log('moving', el.nextSibling);
+						lastInsert.insertBefore(el.nextSibling, null);
+					}
+				}
 			}
 
 			/*
