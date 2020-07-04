0477624d473ceb5a5349780dd51c678f32159ec0
Chris Klimas <chris@twinery.org>
2020-07-04 12:06:26 -0400

    fix: update config text block when state changes
    Resolves #53

--- src/backstage/style/config.jsx
+++ src/backstage/style/config.jsx
@@ -1,6 +1,11 @@
-import {h} from 'preact';
+import {Component, h} from 'preact';
 import {get} from '../../state';
 import Panel from '../panel';
+import event from '../../event';
+
+/*
+State keys we care about.
+*/
 
 const keys = ['page', 'page.header', 'page.footer'].reduce(
 	(out, key) =>
@@ -17,36 +22,62 @@ const keys = ['page', 'page.header', 'page.footer'].reduce(
 	[]
 );
 
-export default function Config() {
-	const output = keys
-		.reduce((out, key) => {
-			const value = get(key);
-
-			if (value) {
-				return out + `${key}: ${JSON.stringify(value)}\n`;
-			}
-
+export default class Config extends Component {
+	constructor(props) {
+		super(props);
+		this.state = keys.reduce((out, key) => {
+			out[key] = get(key);
 			return out;
-		}, '')
-		.trim();
+		}, {});
+		this.onStateChangeBound = ({name, value}) =>
+			this.onStateChange(name, value);
+	}
 
-	return (
-		<Panel title="Config">
-			<p>
-				<label for="cb-backstage-style-config">
-					Enter this code into your first passage&rsquo;s vars section
-					to permanently use this style:
-				</label>
-			</p>
-			<p>
-				<textarea
-					readonly
-					id="cb-backstage-style-config"
-					onClick={e => e.target.select()}
-				>
-					{output}
-				</textarea>
-			</p>
-		</Panel>
-	);
+	onStateChange(name, value) {
+		if (this.state[name]) {
+			this.setState({[name]: value});
+		}
+	}
+
+	render() {
+		const output = Object.keys(this.state)
+			.reduce((out, key) => {
+				const value = get(key);
+
+				if (value) {
+					return out + `${key}: ${JSON.stringify(value)}\n`;
+				}
+
+				return out;
+			}, '')
+			.trim();
+
+		return (
+			<Panel title="Config">
+				<p>
+					<label for="cb-backstage-style-config">
+						Enter this code into your first passage&rsquo;s vars section to
+						permanently use this style:
+					</label>
+				</p>
+				<p>
+					<textarea
+						readonly
+						id="cb-backstage-style-config"
+						onClick={e => e.target.select()}
+					>
+						{output}
+					</textarea>
+				</p>
+			</Panel>
+		);
+	}
+
+	componentWillMount() {
+		event.on('state-change', this.onStateChangeBound);
+	}
+
+	componentWillUnmount() {
+		event.removeListener('state-change', this.onStateChangeBound);
+	}
 }
